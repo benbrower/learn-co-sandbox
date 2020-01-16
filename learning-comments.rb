cb8c4a19b406281046c682d8f36b23786d30260a
mystical-calculator-5374 <bbrower@smu.edu>
2020-01-16 01:51:58 +0000

    Automatically backed up by Learn

--- learning-comments.rb
+++ learning-comments.rb
@@ -1,12 +1,12 @@
-name = "Alice"
+case name
 
-if name == "Alice"
+when "Alice"
   puts "Hello, Alice!"
-elsif name == "The White Rabbit"
+when "The White Rabbit"
   puts "Don't be late, White Rabbit"
-elsif name == "The Mad Hatter"
+when "The Mad Hatter"
   puts "Welcome to the tea party, Mad Hatter"
-elsif name == "The Queen of Hearts"
+when "The Queen of Hearts"
   puts "Please don't chop off my head!"
 else 
   puts "Who are you?"
